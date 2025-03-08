030f67e346cd4488aa1aaf17afdc9cf74a85fa83
Ender Puentes <endpuent@gmail.com>
2025-03-08 17:07:15 -0300

    feat: improve simulator UI and add configuration component integration

--- src/components/simulator.tsx
+++ src/components/simulator.tsx
@@ -1,83 +1,92 @@
 "use client";
 
 import { ProcessSchedulerSimulator } from "@/lib/simulator";
-import { Process, SimulatorState } from "@/lib/types";
+import { Process, SimulatorConfig, SimulatorState } from "@/lib/types";
 import { useEffect, useState } from "react";
+import { ProcessSchedulerConfiguration } from "./simulator/configuration";
 import { Button } from "./ui/button";
 
 export function ProcessSchedulerSimulatorVisualizer() {
   const [simulator, setSimulator] = useState<ProcessSchedulerSimulator | null>(
     null
   );
-  const [currentTime, setCurrentTime] = useState(0);
-  const [currentState, setCurrentState] = useState<SimulatorState>(
-    SimulatorState.STOPPED
-  );
-  const [currentProcess, setCurrentProcess] = useState<Process | null>(null);
+  const [time, setTime] = useState(0);
+  const [state, setState] = useState<SimulatorState>(SimulatorState.STOPPED);
+  const [process, setProcess] = useState<Process | null>(null);
   const [processes, setProcesses] = useState<Process[]>([]);
+  const [config, setConfig] = useState<SimulatorConfig | null>(null);
 
   useEffect(() => {
     const simulatorInstance = new ProcessSchedulerSimulator();
     setSimulator(simulatorInstance);
+    setConfig(simulatorInstance.getConfig());
 
     simulatorInstance.subscribe(() => {
-      setCurrentState(simulatorInstance.getCurrentState());
-      setCurrentTime(simulatorInstance.getCurrentTime());
+      setState(simulatorInstance.getCurrentState());
+      setTime(simulatorInstance.getCurrentTime());
+      setProcess(simulatorInstance.getCurrentProcess());
+      setProcesses(simulatorInstance.getProcesses());
+      setConfig(simulatorInstance.getConfig());
     });
   }, []);
 
-  const handleStart = () => {
-    simulator?.start();
-  };
   return (
-    <section className="flex flex-col gap-4">
-      {/* <ProcessSchedulerConfiguration
-        simulatorConfig={simulatorConfig}
-        setSimulatorConfig={setSimulatorConfig}
-      /> */}
-      Agregamos el control de inicio, pausa y reset del simulador
-      <div className="flex flex-row gap-4">
-        <Button
-          disabled={currentState === SimulatorState.RUNNING}
-          onClick={() =>
-            currentState === SimulatorState.STOPPED
-              ? simulator?.start()
-              : simulator?.restart()
-          }
-        >
-          {currentState === SimulatorState.STOPPED ? "Iniciar" : "Reanudar"}
-        </Button>
-        <Button
-          disabled={currentState !== SimulatorState.RUNNING}
-          onClick={() => simulator?.pause()}
-        >
-          Pausar
-        </Button>
-        <Button
-          disabled={currentState !== SimulatorState.PAUSED}
-          onClick={() => simulator?.reset()}
-        >
-          Reiniciar
-        </Button>
-      </div>
-      <div className="flex flex-row gap-4">
-        <h1>Current State: {currentState}</h1>
-        <h1>Current Time: {currentTime}</h1>
-        <h1>Current Process: {currentProcess?.id || "N/A"}</h1>
-        <h1>Processes: {processes.map((process) => process.id).join(", ")}</h1>
-      </div>
-      <div className="flex flex-row gap-4">
-        <h1>Processes: </h1>
-        {processes.length > 0 ? (
-          processes.map((process) => (
-            <div key={process.id}>
-              <p>{process.id}</p>
-              <p>{process.state}</p>
-            </div>
-          ))
-        ) : (
-          <p>Process not found</p>
+    <section className="flex flex-col gap-4 w-full">
+      <div className="container">
+        {config && (
+          <ProcessSchedulerConfiguration
+            config={config}
+            updateConfig={(newConfig: SimulatorConfig) =>
+              simulator?.updateConfig(newConfig)
+            }
+          />
         )}
+        <div className="flex flex-row gap-4">
+          <Button
+            disabled={state === SimulatorState.RUNNING}
+            onClick={() =>
+              state === SimulatorState.STOPPED
+                ? simulator?.start()
+                : simulator?.restart()
+            }
+          >
+            {state === SimulatorState.STOPPED ? "Iniciar" : "Reanudar"}
+          </Button>
+          <Button
+            disabled={state !== SimulatorState.RUNNING}
+            onClick={() => simulator?.pause()}
+          >
+            Pausar
+          </Button>
+          <Button
+            disabled={state !== SimulatorState.PAUSED}
+            onClick={() => simulator?.reset()}
+          >
+            Reiniciar
+          </Button>
+        </div>
+        <div className="flex flex-row gap-4">
+          <h1>State: {state}</h1>
+          <h1>Time: {time}</h1>
+          <h1>Process: {process?.id || "N/A"}</h1>
+        </div>
+        <div className="flex flex-col gap-4">
+          <h1>Processes: </h1>
+          <div className="flex flex-col gap-4">
+            {processes.length > 0 ? (
+              processes.map((process) => (
+                <div key={process.id} className="flex flex-row gap-4">
+                  <p>{process.id}</p>
+                  <p>{process.state}</p>
+                  <p>{process.priority}</p>
+                  
+                </div>
+              ))
+            ) : (
+              <p>Process not found</p>
+            )}
+          </div>
+        </div>
       </div>
     </section>
   );
