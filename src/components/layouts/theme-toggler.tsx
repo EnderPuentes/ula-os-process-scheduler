50c081687f4cfa7bb89b702cbe9415d252a15a66
Ender Puentes <endpuent@gmail.com>
2025-03-08 17:18:26 -0300

    chore: remove unnecessary Suspense wrapper from theme toggler

--- src/components/layouts/theme-toggler.tsx
+++ src/components/layouts/theme-toggler.tsx
@@ -2,7 +2,6 @@
 
 import { MoonIcon, SunDimIcon } from "lucide-react";
 import { useTheme } from "next-themes";
-import { Suspense } from "react";
 
 export function ThemeToggler() {
   const { resolvedTheme, setTheme } = useTheme();
@@ -12,17 +11,15 @@ export function ThemeToggler() {
   };
 
   return (
-    <Suspense fallback={null}>
-      <button
-        onClick={toggleTheme}
-        className="flex items-center justify-center w-10 h-10 rounded-full bg-background cursor-pointer"
-      >
-        {resolvedTheme === "light" ? (
-          <MoonIcon className="w-4 h-4" />
-        ) : (
-          <SunDimIcon className="w-6 h-6" />
-        )}
-      </button>
-    </Suspense>
+    <button
+      onClick={toggleTheme}
+      className="flex items-center justify-center w-10 h-10 rounded-full bg-background cursor-pointer"
+    >
+      {resolvedTheme === "light" ? (
+        <MoonIcon className="w-4 h-4" />
+      ) : (
+        <SunDimIcon className="w-6 h-6" />
+      )}
+    </button>
   );
 }
