be2cf543ead38a1ea9a2d3ec90b43a69d903b8ed
Ender Puentes <endpuent@gmail.com>
2025-03-08 22:55:52 -0300

    refactor: restructure SimulatorVisualizer with improved layout and component organization

--- src/components/simulator.tsx
+++ src/components/simulator.tsx
@@ -5,15 +5,15 @@ import { Process, SimulatorConfig, SimulatorState } from "@/lib/types";
 import { useEffect, useState } from "react";
 import { SimulatorConfiguration } from "./simulator/configuration";
 import { SimulatorProcessControl } from "./simulator/process-control";
-import { Button } from "./ui/button";
+import { SimulatorProcessor } from "./simulator/processor";
 
 export function SimulatorVisualizer() {
   const [simulator, setSimulator] = useState<ProcessSchedulerSimulator | null>(
     null
   );
-  const [time, setTime] = useState(0);
+  const [tick, setTick] = useState(0);
   const [state, setState] = useState<SimulatorState>(SimulatorState.STOPPED);
-  const [process, setProcess] = useState<Process | null>(null);
+  const [currentProcess, setCurrentProcess] = useState<Process | null>(null);
   const [processes, setProcesses] = useState<Process[]>([]);
   const [config, setConfig] = useState<SimulatorConfig | null>(null);
 
@@ -24,55 +24,41 @@ export function SimulatorVisualizer() {
 
     simulatorInstance.subscribe(() => {
       setState(simulatorInstance.getCurrentState());
-      setTime(simulatorInstance.getCurrentTime());
-      setProcess(simulatorInstance.getCurrentProcess());
+      setTick(simulatorInstance.getCurrentTick());
+      setCurrentProcess(simulatorInstance.getCurrentProcess());
       setProcesses(simulatorInstance.getProcesses());
       setConfig(simulatorInstance.getConfig());
     });
   }, []);
 
   return (
-    <section className="flex flex-col gap-4 w-full">
+    <section className="flex flex-col gap-4 w-full min-h-screen py-10">
       <div className="container">
-        {config && (
-          <SimulatorConfiguration
-            config={config}
-            updateConfig={(newConfig: SimulatorConfig) =>
-              simulator?.updateConfig(newConfig)
-            }
-          />
-        )}
-        <div className="flex flex-row gap-4">
-          <Button
-            disabled={state === SimulatorState.RUNNING}
-            onClick={() =>
-              state === SimulatorState.STOPPED
-                ? simulator?.start()
-                : simulator?.restart()
-            }
-          >
-            {state === SimulatorState.STOPPED ? "Iniciar" : "Reanudar"}
-          </Button>
-          <Button
-            disabled={state !== SimulatorState.RUNNING}
-            onClick={() => simulator?.pause()}
-          >
-            Pausar
-          </Button>
-          <Button
-            disabled={state !== SimulatorState.PAUSED}
-            onClick={() => simulator?.reset()}
-          >
-            Reiniciar
-          </Button>
+        <div className="grid grid-cols-[280px_1fr] gap-4">
+          <div className="flex flex-col gap-4">
+            <SimulatorProcessor
+              state={state}
+              tick={tick}
+              tickSpeed={config?.processor.tickSpeed ?? 0}
+              start={() => simulator?.start()}
+              pause={() => simulator?.pause()}
+              reset={() => simulator?.reset()}
+              restart={() => simulator?.restart()}
+              currentProcess={currentProcess}
+            />
+            {config && (
+              <SimulatorConfiguration
+                config={config}
+                updateConfig={(newConfig: SimulatorConfig) =>
+                  simulator?.updateConfig(newConfig)
+                }
+              />
+            )}
+          </div>
+          <div className="flex flex-col gap-4">
+            <SimulatorProcessControl processes={processes} />
+          </div>
         </div>
-        <div className="flex flex-row gap-4">
-          <h1>State: {state}</h1>
-          <h1>Time: {time}</h1>
-          <h1>Process: {process?.id || "N/A"}</h1>
-        </div>
-
-        <SimulatorProcessControl processes={processes} />
       </div>
     </section>
   );
