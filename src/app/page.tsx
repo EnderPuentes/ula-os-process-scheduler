721669be33609852222d3cddfe0fe1e007724945
Ender Puentes <endpuent@gmail.com>
2025-03-08 22:56:03 -0300

    refactor: simplify HomePage component layout for SimulatorVisualizer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,5 @@
 import { SimulatorVisualizer } from "@/components/simulator";
 
 export default function HomePage() {
-  return (
-    <section className="flex flex-col items-center justify-start min-h-screen py-10">
-      <SimulatorVisualizer />
-    </section>
-  );
+  return <SimulatorVisualizer />;
 }
