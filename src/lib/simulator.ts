127f58a4abbd31978587cba181aa28e87132d343
Ender Puentes <endpuent@gmail.com>
2025-03-08 22:55:09 -0300

    feat: implement process scheduling simulator core logic with tick-based simulation

--- src/lib/simulator.ts
+++ src/lib/simulator.ts
@@ -1,3 +1,4 @@
+import { SchedulerProcessAlgorithms } from "./algorithms";
 import {
   Process,
   ProcessState,
@@ -7,11 +8,13 @@ import {
 } from "./types";
 
 export class ProcessSchedulerSimulator {
+  private algorithms: SchedulerProcessAlgorithms;
+
   private processes: Process[] = [];
   private currentProcess: Process | null = null;
 
   private timer: NodeJS.Timeout | null = null;
-  private currentTime: number = 0;
+  private currentTick: number = 0;
 
   private state: SimulatorState = SimulatorState.STOPPED;
 
@@ -21,9 +24,8 @@ export class ProcessSchedulerSimulator {
       quantum: 10,
     },
     processes: {
-      maxBurstTime: 250,
       maxPriority: 5,
-      maxArrivalTime: 50,
+      maxBurstTick: 10,
     },
     processor: {
       tickSpeed: 1000,
@@ -34,24 +36,26 @@ export class ProcessSchedulerSimulator {
 
   constructor(config?: SimulatorConfig | null) {
     this.config = config || this.config;
+    this.algorithms = new SchedulerProcessAlgorithms();
   }
 
   private generateRandomProcess(): Process {
+    const executionTick =
+      Math.floor(Math.random() * this.config.processes.maxBurstTick) + 1;
+
     const newProcess: Process = {
       id: this.processes.length + 1,
-      arrivalTime: Math.floor(
-        Math.random() * this.config.processes.maxArrivalTime
-      ) + 1,
-      burstTime: Math.floor(Math.random() * this.config.processes.maxBurstTime) + 1,
-      priority: Math.floor(Math.random() * this.config.processes.maxPriority) + 1,
+      arrivalTick: this.currentTick,
+      burstTick: executionTick,
+      remainingTick: executionTick,
+      priority:
+        Math.floor(Math.random() * this.config.processes.maxPriority) + 1,
       state: ProcessState.READY,
-      remainingIoTime: 0,
-      waitingTime: 0,
-      turnaroundTime: 0,
-      responseTime: 0,
-      blockingTime: 0,
-      completionTime: 0,
-      remainingTime: 0,
+      waitingTick: 0,
+      turnaroundTick: 0,
+      responseTick: null,
+      blockingTick: 0,
+      completionTick: null,
     };
     return newProcess;
   }
@@ -68,17 +72,17 @@ export class ProcessSchedulerSimulator {
     if (this.state === SimulatorState.STOPPED) {
       this.state = SimulatorState.RUNNING;
 
-      // Add 10 random processes to the queue
-      for (let i = 0; i < 10; i++) {
-        this.processes.push(this.generateRandomProcess());
-      }
+      this.processes.push(this.generateRandomProcess());
+      this.notify();
 
       this.timer = setInterval(() => {
-        this.currentTime++;
+        this.scheduleProcess();
+        this.currentTick++;
+        this.updateProcesses();
 
-        if (Math.random() < 0.5) {
-          // Add random processes to the queue every 5 ticks
-          for (let i = 0; i < Math.floor(Math.random() * 5); i++) {
+        // Generate random processes with a 25% chance 1-5 processes
+        for (let i = 0; i < Math.random() * 5; i++) {
+          if (Math.random() < 0.25) {
             this.processes.push(this.generateRandomProcess());
           }
         }
@@ -93,7 +97,9 @@ export class ProcessSchedulerSimulator {
       this.state = SimulatorState.RUNNING;
 
       this.timer = setInterval(() => {
-        this.currentTime++;
+        this.scheduleProcess();
+        this.currentTick++;
+        this.updateProcesses();
         this.notify();
       }, this.config.processor.tickSpeed);
     }
@@ -122,8 +128,67 @@ export class ProcessSchedulerSimulator {
       }
 
       this.processes = [];
-      this.currentTime = 0;
+      this.currentTick = 0;
       this.currentProcess = null;
+      this.notify();
+    }
+  }
+
+  private updateProcesses() {
+    this.processes
+      .filter((process) => process.state !== ProcessState.COMPLETED)
+      .forEach((process) => {
+        switch (process.state) {
+          case ProcessState.RUNNING:
+            process.remainingTick--;
+            if (process.remainingTick <= 0) {
+              process.state = ProcessState.COMPLETED;
+              process.completionTick = this.currentTick;
+            }
+            process.turnaroundTick++;
+            break;
+          case ProcessState.READY:
+            process.waitingTick++;
+            process.turnaroundTick++;
+            break;
+        }
+      });
+  }
+
+  private updateProcess(process: Process) {
+    this.processes.forEach((p) => {
+      if (p.id === process.id) {
+        p = process;
+      }
+    });
+  }
+
+  private scheduleProcess(): void {
+    // Get the next process to run
+    const nextProcess = this.algorithms.getAlgorithm(
+      this.config.algorithm.type
+    )(this.processes, this.currentProcess);
+
+    if (nextProcess) {
+      // If the current process is running, set it to blocked
+      if (this.currentProcess?.state === ProcessState.RUNNING) {
+        this.updateProcess({
+          ...this.currentProcess,
+          state: ProcessState.BLOCKED,
+        });
+      }
+
+      // Set the current process to the next process
+      this.currentProcess = nextProcess;
+
+      // Update the state of the current process
+      if (this.currentProcess) {
+        this.currentProcess.state = ProcessState.RUNNING;
+        this.currentProcess.responseTick = this.currentTick;
+        this.updateProcess(this.currentProcess);
+      }
+
+      // Notify the listeners
       this.notify();
     }
   }
@@ -132,8 +197,8 @@ export class ProcessSchedulerSimulator {
     return this.state;
   }
 
-  public getCurrentTime(): number {
-    return this.currentTime;
+  public getCurrentTick(): number {
+    return this.currentTick;
   }
 
   public getCurrentProcess(): Process | null {
