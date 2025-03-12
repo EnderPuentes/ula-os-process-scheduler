e253acf766f35dede6635ebad4f7101352d09422
Ender Puentes <endpuent@gmail.com>
2025-03-11 23:59:45 -0300

    style(header): update logo text and size with playful emoji

--- src/components/layouts/header.tsx
+++ src/components/layouts/header.tsx
@@ -6,7 +6,7 @@ export function Header() {
     <header className="sticky top-0 bg-transparent backdrop-blur-lg z-50 shadow-sm h-20 flex justify-center items-center">
       <div className="container flex justify-between items-center">
         <Link href="/">
-          <h1 className="text-xl font-bold">Scheduler Simulator</h1>
+          <h1 className="text-2xl font-bold">👾Process Scheduler_</h1>
         </Link>
         <ThemeToggler />
       </div>
